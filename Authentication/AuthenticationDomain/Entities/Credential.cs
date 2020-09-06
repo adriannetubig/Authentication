48cffe6bff3464945e387bd5688f20d4c262eb9d
Adrianne Claude Tubig - MSI <adriannetubig@gmail.com>
2020-09-07 00:31:05 +0800

    Created Domain Project

---
+++ Authentication/AuthenticationDomain/Entities/Credential.cs
@@ -0,0 +1,49 @@
+﻿using CSharpFunctionalExtensions;
+using System.Text.RegularExpressions;
+
+namespace AuthenticationDomain.Entities
+{
+    public class Credential: Entity
+    {
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public bool IsActive { get; private set; }
+
+        protected Credential()
+        {
+        }
+
+        private Credential(string username, string password)
+        {
+            Username = username;
+            Password = BCrypt.Net.BCrypt.HashPassword(password);
+            IsActive = true;
+        }
+
+        public static Result<Credential> Create(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username))
+                return Result.Failure<Credential>("Username Required");
+            else if (username.Length < 6)
+                return Result.Failure<Credential>("Minimum of 6 characters for Username");
+            else if (username.Length > 15)
+                return Result.Failure<Credential>("Maximum of 15 characters for Username");
+            else if (!Regex.IsMatch(username, @"^[a-zA-Z0-9_]+$"))
+                return Result.Failure<Credential>("Alphanumeric characters and underscore are allowed for Username");
+
+            if (string.IsNullOrEmpty(password))
+                return Result.Failure<Credential>("Password Required");
+
+            return Result.Success(new Credential(username, password));
+
+        }
+
+        public bool Authenticate(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return false;
+
+            return BCrypt.Net.BCrypt.Verify(password, Password);
+        }
+    }
+}
---
+++ Authentication/AuthenticationDomainTest/Entities/CredentialTest.cs
@@ -0,0 +1,224 @@
+﻿using AuthenticationDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AuthenticationDomainTest.Entities
+{
+    public class CredentialTest
+    {
+        private const string _username = "Username";
+        private const string _password = "Password";
+
+        [Fact]
+        public void Create()
+        {
+            var credentialResult = Credential.Create(_username, _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Success_IsActiveIsAlwaysTrue()
+        {
+            var credentialResult = Credential.Create(_username, _password);
+
+            Assert.True(credentialResult.IsSuccess);
+            Assert.True(credentialResult.Value.IsActive);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameShouldBeReflected()
+        {
+            var credentialResult = Credential.Create(_username, _password);
+
+            Assert.True(credentialResult.IsSuccess);
+            Assert.Equal(_username, credentialResult.Value.Username);
+        }
+
+        [Fact]
+        public void Create_Fail_UserNameIsNull()
+        {
+            var credentialResult = Credential.Create(null, _password);
+
+            Assert.True(credentialResult.IsFailure);
+        }
+
+        [Fact]
+        public void Create_Fail_UserNameIsEmptyString()
+        {
+            var credentialResult = Credential.Create(string.Empty, _password);
+
+            Assert.True(credentialResult.IsFailure);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameIsMinimumCharacter()
+        {            
+            var credentialResult = Credential.Create("userna", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Fail_UsernameIsBelowMinimumCharacter()
+        {
+            var credentialResult = Credential.Create("usern", _password);
+
+            Assert.True(credentialResult.IsFailure);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameIsMaximumCharacter()
+        {
+            var credentialResult = Credential.Create("username9012345", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Fail_UsernameIsAboveMaximumCharacter()
+        {
+            var credentialResult = Credential.Create("username90123456", _password);
+
+            Assert.True(credentialResult.IsFailure);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameProceedingUnderscore()
+        {
+            var credentialResult = Credential.Create("_username", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameUnderscore()
+        {
+            var credentialResult = Credential.Create("user_name", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameFollowingUnderscore()
+        {
+            var credentialResult = Credential.Create("username_", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameProceedingNumber()
+        {
+            var credentialResult = Credential.Create("0username", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameNumber()
+        {
+            var credentialResult = Credential.Create("user1name", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Success_UsernameFollowingNumber()
+        {
+            var credentialResult = Credential.Create("username5", _password);
+
+            Assert.True(credentialResult.IsSuccess);
+        }
+
+        [Fact]
+        public void Create_Fail_UsernameHasOtherSpecialCharacters()
+        {
+            Assert.True(Credential.Create("u`sername", _password).IsFailure);
+            Assert.True(Credential.Create("u~sername", _password).IsFailure);
+            Assert.True(Credential.Create("u!sername", _password).IsFailure);
+            Assert.True(Credential.Create("u@sername", _password).IsFailure);
+            Assert.True(Credential.Create("u#sername", _password).IsFailure);
+            Assert.True(Credential.Create("u$sername", _password).IsFailure);
+            Assert.True(Credential.Create("u%sername", _password).IsFailure);
+            Assert.True(Credential.Create("u^sername", _password).IsFailure);
+            Assert.True(Credential.Create("u&sername", _password).IsFailure);
+            Assert.True(Credential.Create("u*sername", _password).IsFailure);
+            Assert.True(Credential.Create("u(sername", _password).IsFailure);
+            Assert.True(Credential.Create("u)sername", _password).IsFailure);
+            Assert.True(Credential.Create("u-sername", _password).IsFailure);
+            Assert.True(Credential.Create("u=sername", _password).IsFailure);
+            Assert.True(Credential.Create("u+sername", _password).IsFailure);
+            Assert.True(Credential.Create("u[sername", _password).IsFailure);
+            Assert.True(Credential.Create("u{sername", _password).IsFailure);
+            Assert.True(Credential.Create("u]sername", _password).IsFailure);
+            Assert.True(Credential.Create("u}sername", _password).IsFailure);
+            Assert.True(Credential.Create("u\\sername", _password).IsFailure);
+            Assert.True(Credential.Create("u|sername", _password).IsFailure);
+            Assert.True(Credential.Create("u;sername", _password).IsFailure);
+            Assert.True(Credential.Create("u:sername", _password).IsFailure);
+            Assert.True(Credential.Create("u'sername", _password).IsFailure);
+            Assert.True(Credential.Create("u\"sername", _password).IsFailure);
+            Assert.True(Credential.Create("u,sername", _password).IsFailure);
+            Assert.True(Credential.Create("u<sername", _password).IsFailure);
+            Assert.True(Credential.Create("u.sername", _password).IsFailure);
+            Assert.True(Credential.Create("u>sername", _password).IsFailure);
+            Assert.True(Credential.Create("u/sername", _password).IsFailure);
+            Assert.True(Credential.Create("u?sername", _password).IsFailure);
+        }
+
+        [Fact]
+        public void Create_Success_PasswordShouldBeEncrypted()
+        {
+            var credentialResult = Credential.Create(_username, _password);
+
+            Assert.True(credentialResult.IsSuccess);
+            Assert.NotEqual(credentialResult.Value.Password, _password);
+        }
+
+        [Fact]
+        public void Create_Fail_PasswordIsNull()
+        {
+            var credentialResult = Credential.Create(_username, null);
+
+            Assert.True(credentialResult.IsFailure);
+        }
+
+        [Fact]
+        public void Create_Fail_PasswordIsEmptyString()
+        {
+            var credentialResult = Credential.Create(_username, string.Empty);
+
+            Assert.True(credentialResult.IsFailure);
+        }
+
+        [Fact]
+        public void Authenticate_Success()
+        {
+            var credential = Credential.Create(_username, _password).Value;
+            var authenticate = credential.Authenticate(_password);
+
+            Assert.True(authenticate);
+        }
+
+        [Fact]
+        public void Authenticate_Fail_SuppliedPasswordIsNull()
+        {
+            var credential = Credential.Create(_username, _password).Value;
+            var authenticate = credential.Authenticate(null);
+
+            Assert.False(authenticate);
+        }
+
+        [Fact]
+        public void Authenticate_Fail_SuppliedPasswordIsEmptyString()
+        {
+            var credential = Credential.Create(_username, _password).Value;
+            var authenticate = credential.Authenticate(string.Empty);
+
+            Assert.False(authenticate);
+        }
+    }
+}
